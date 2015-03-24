7fc56e30cb86fa1c3b0903de33b7b10ef50c6dc0
Bob Spunt <bobspunt@gmail.com>
2015-03-24 12:22:09 -0700

    initial commit

---
+++ setargs.m
@@ -0,0 +1,68 @@
+function argstruct = setargs(defaults, optargs)
+% SETARGS Name/value parsing and assignment of varargin with default values
+%
+% This is a utility for setting the value of optional arguments to a
+% function. The first argument is required and should be a cell array of
+% "name, default value" pairs for all optional arguments. The second
+% argument is optional and should be a cell array of "name, custom value"
+% pairs for at least one of the optional arguments.
+%
+%  USAGE: argstruct = setargs(defaults, args)  
+% __________________________________________________________________________
+%  OUTPUT
+%
+% 	argstruct: structure containing the final argument values
+% __________________________________________________________________________
+%  INPUTS
+%
+% 	defaults:  
+%       cell array of "name, default value" pairs for all optional arguments
+%
+% 	optargs [optional]     
+%       cell array of "name, custom value" pairs for at least one of the
+%       optional arguments. this will typically be the "varargin" array. 
+% __________________________________________________________________________
+%  USAGE EXAMPLE (WITHIN FUNCTION)
+%
+%     defaults    = {   'arg1',     0,      ...
+%                       'arg2',     'zero', ...    
+%                       'arg3',     rand    ...
+%                   };
+%     argstruct = setargs(defaults, varargin)
+%
+
+% ---------------------- Copyright (C) 2015 Bob Spunt ----------------------
+%	Created:  2015-03-11
+%	Email:    spunt@caltech.edu
+% __________________________________________________________________________
+if nargin < 1, mfile_showhelp; return; end
+if nargin < 2, optargs = []; end
+defaults = reshape(defaults, 2, length(defaults)/2)'; 
+if ~isempty(optargs)
+    if mod(length(optargs), 2)
+        error('Optional inputs must be entered as Name, Value pairs, e.g., myfunction(''name'', value)'); 
+    end
+    arg = reshape(optargs, 2, length(optargs)/2)';
+    for i = 1:size(arg,1)
+       idx = strncmpi(defaults(:,1), arg{i,1}, length(arg{i,1}));
+       if sum(idx) > 1
+           error(['Input "%s" matches multiple valid inputs:' repmat('  %s', 1, sum(idx))], arg{i,1}, defaults{idx, 1});
+       elseif ~any(idx)
+           error('Input "%s" does not match a valid input.', arg{i,1});
+       else
+           defaults{idx,2} = arg{i,2};
+       end  
+    end
+end
+for i = 1:size(defaults,1), assignin('caller', defaults{i,1}, defaults{i,2}); end
+if nargout>0, argstruct = cell2struct(defaults(:,2), defaults(:,1)); end
+end
+% =========================================================================
+% * SUBFUNCTIONS
+% =========================================================================
+function mfile_showhelp(varargin)
+% MFILE_SHOWHELP
+ST = dbstack('-completenames');
+if isempty(ST), fprintf('\nYou must call this within a function\n\n'); return; end
+eval(sprintf('help %s', ST(2).file));  
+end
